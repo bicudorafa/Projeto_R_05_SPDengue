fa387cdf374451e6c550d36bbd1e58f7fe3a47d7
Rafael Bicudo <rafaelbicudo.r@outlook.com>
2018-07-31 21:53:14 -0300

    Base de dados e 1o script funcional para limpeza dos dados.

--- Projeto_R_05_SPDengue.R
+++ Projeto_R_05_SPDengue.R
@@ -1,1 +1,29 @@
 ### Dengue
+
+# Fonte: http://tabnet.datasus.gov.br/cgi/deftohtm.exe?sinannet/cnv/dengueSP.def
+library(dplyr)
+
+dengue2008 <- read.csv('dengue_sp_2007.csv', sep = ';', skip = 3, stringsAsFactors = F, na.strings
+= '-', header = T, blank.lines.skip = TRUE)
+dengue2008_tbl <- dengue2008 %>% as.tbl() %>% slice(1:526)
+head(dengue2008)
+glimpse(dengue2008)
+
+# Data Cleaning
+library(tidyr)
+library(stringr)
+
+# tidy
+dengue2008_t <- dengue2008_tbl %>% 
+  select(-Total) %>% 
+  gather(key = Regiao, value = Casos, -Município.de.notificação, na.rm = T)
+names(dengue2008_t) <- c('Municipio', 'Regiao', 'Casos')
+
+# Data Cleaning
+dengue2008_clean <- dengue2008_t
+
+# Municipio
+dengue2008_clean$Municipio <- str_replace_all(dengue2008_t$Municipio, '[:digit:]', '')
+
+# Regiao
+dengue2008_clean$Regiao <- str_replace_all(dengue2008_clean$Regiao, '[:digit:]|[X.]', ' ')
